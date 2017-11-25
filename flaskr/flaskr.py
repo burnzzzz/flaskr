eaaba09f3eac527389cce7aa462c8451a61a4b0f
kkalynovskyi <kkalinovskiy@gmail.com>
2017-11-25 16:21:04 +0200

    testing pipeline 1

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -25,7 +25,7 @@ app.config.update(dict(
     DEBUG=True,
     SECRET_KEY='development key',
     USERNAME='admin',
-    PASSWORD='12356789'
+    PASSWORD='1235678'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 

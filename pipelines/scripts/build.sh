62a297db4d893da5f30a7b5cfafcf3f0075c0a0c
Consatntine Kalinovskiy <kkalynovskyi@mirantis.com>
2017-11-26 23:19:05 +0200

    Commenting image remove for testing purposes

--- pipelines/scripts/build.sh
+++ pipelines/scripts/build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 set -x
+set -e
 
 IMAGE_NAME_APACHE=${IMAGE_NAME_APACHE:-flaskr}
 REGISTRY_URL=${REGISTRY_URL:-kkalynovskyi}
 TAG=${TAG_APACHE:-latest}
 docker build -t "${REGISTRY_URL}"/"${IMAGE_NAME_APACHE}":"${TAG}" \
 		 	 -f Dockerfile .
-docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}" https://"${REGISTRY_URL}"
-set +x
-docker push "${REGISTRY_URL}"/"${IMAGE_NAME_APACHE}":"${TAG}"
-set -x
-docker rm "${REGISTRY_URL}"/"${IMAGE_NAME_APACHE}":"${TAG}"
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+set +x; docker push "${REGISTRY_URL}"/"${IMAGE_NAME_APACHE}":"${TAG}"; set -x
+#docker rmi "${REGISTRY_URL}"/"${IMAGE_NAME_APACHE}":"${TAG}"
 

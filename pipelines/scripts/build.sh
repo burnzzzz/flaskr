9a7074e333b735ce48152ae24d0ceb361ae8a713
Consatntine Kalinovskiy <kkalynovskyi@mirantis.com>
2017-12-04 23:57:03 +0200

    adding helm chart definition

--- pipelines/scripts/build.sh
+++ pipelines/scripts/build.sh
@@ -3,11 +3,11 @@
 set +x
 set -e
 
-IMAGE_NAME_APACHE=${IMAGE_NAME_APACHE:-flaskr}
+IMAGE_NAME=${IMAGE_NAME:-flaskr}
 REGISTRY_URL=${REGISTRY_URL:-kkalynovskyi}
 TAG=${TAG_APACHE:-latest}
-docker build -t "${REGISTRY_URL}"/"${IMAGE_NAME_APACHE}":"${TAG}" \
+docker build -t "${REGISTRY_URL}"/"${IMAGE_NAME}":"${TAG}" \
 		 	 -f Dockerfile .
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
-docker push "${REGISTRY_URL}"/"${IMAGE_NAME_APACHE}":"${TAG}"
-#docker rmi --force "${REGISTRY_URL}"/"${IMAGE_NAME_APACHE}":"${TAG}"
+docker push "${REGISTRY_URL}"/"${IMAGE_NAME}":"${TAG}"
+#docker rmi --force "${REGISTRY_URL}"/"${IMAGE_NAME}":"${TAG}"
